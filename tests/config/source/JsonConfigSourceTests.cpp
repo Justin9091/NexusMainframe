573a67a0805ac7afea03e51dc564f4e039c81283
Justin9091 <jusraaijmakers@gmail.com>
2026-01-10 09:50:49 +0100

    added tests

--- tests/config/source/JsonConfigSourceTests.cpp
+++ tests/config/source/JsonConfigSourceTests.cpp
@@ -1,3 +1,25 @@
 //
 // Created by jusra on 10-1-2026.
 //
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Placeholder test", "[placeholder]") {
+    REQUIRE(true);
+
+    SECTION("Basic arithmetic works") {
+        REQUIRE(1 + 1 == 2);
+        REQUIRE(2 * 2 == 4);
+    }
+
+    SECTION("String comparison works") {
+        std::string hello = "Hello";
+        REQUIRE(hello == "Hello");
+        REQUIRE(hello.length() == 5);
+    }
+}
+
+TEST_CASE("Another placeholder", "[placeholder]") {
+    CHECK(42 == 42);
+    CHECK_FALSE(1 == 2);
+}
